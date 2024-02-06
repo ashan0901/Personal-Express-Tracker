bb0c281b9faf108470c2b121444e8ea7ace9c87d
ashan0901 <ashanchanuka1211@gmail.com>
2024-02-06 22:50:17 +0530

    Ashan-MongoDB Changed

--- Personal-Expense-Tracker-Backend/src/main/java/com/faward/walletapp/repository/WallRepository.java
+++ Personal-Expense-Tracker-Backend/src/main/java/com/faward/walletapp/repository/WallRepository.java
@@ -1,12 +1,13 @@
 package com.faward.walletapp.repository;
 
 import com.faward.walletapp.entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface WallRepository extends JpaRepository<Wallet,Long> {
+public interface WallRepository extends MongoRepository<Wallet,Long> {
     List<Wallet>findAllByOrderByPriority();
 }
