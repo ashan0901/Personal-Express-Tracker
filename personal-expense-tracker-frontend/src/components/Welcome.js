395e749eee319dfb549d6a12d3cedd89371b393b
Deesha Sewmini <deeshasewmini@gmail.com>
2024-04-15 11:34:36 +0530

    welcome page created

--- personal-expense-tracker-frontend/src/components/Welcome.js
+++ personal-expense-tracker-frontend/src/components/Welcome.js
@@ -1,1 +1,38 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Nav from "./shared/Nav";
 
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <Nav />
+        <div className="light-overlay landing-inner text-dark">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Personal Expense Manager</h1>
+                <p className="lead">
+                  Create your account to manage your daily expense and income
+                </p>
+                <hr />
+                <Link
+                  to="/register"
+                  className="btn btn-lg btn-primary mr-2"
+                  style={{ marginRight: "20px" }}
+                >
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-secondary mr-2">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
